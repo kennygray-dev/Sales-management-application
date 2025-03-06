f260e8ca10558066dab6c59761dd7027258a4d3b
kennygray-dev <thekennygray@gmail.com>
2025-03-06 19:11:10 +0100

    Initial commit

---
+++ Dakestel-Dashboard/backend/src/types.d.ts
@@ -0,0 +1,37 @@
+import { Request } from "express";
+import { IUser } from "./models/userModel";
+
+export interface CustomRequest extends Request {
+  user?: {
+    id: string;
+    name: string;
+    email: string;
+  };
+
+  user?: IUser;
+}
+
+export interface Product {
+  _id: string;
+  name: string;  // Ensure the name property exists
+  quantity: number;
+  price: number; 
+}
+
+
+export interface Order {
+  _id: string;
+  customerName: string;
+  customerEmail: string;
+  customerPhone: string;
+  totalAmount: number;
+  status: "pending" | "completed";
+  products: {
+    product: {
+      _id: string;
+      name: string;
+    } | string; 
+    quantity: number;
+  }[];
+}
+
